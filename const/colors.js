6953e55e3ee531c9c2f5322e5de2cc37a8738150
Rama Aditya Maulana <ramaadtym@gmail.com>
2022-09-18 19:51:31 +0700

    Mapping Color variable

---
+++ const/colors.js
@@ -0,0 +1,100 @@
+export const orange = {
+  $primary: "#ff9738",
+  "$orange-1": "#fffbf7",
+  "$orange-2": "#ffefe2",
+  "$orange-3": "#ffa000",
+  "$orange-4": "#ff6400",
+};
+
+export const blue = {
+  "$primary-blue": "#192383",
+  "$blue-1": "#7097c7",
+  "$blue-2": "#3062a1",
+  "$blue-3": "#1253a4",
+  "$blue-4": "#11417c",
+};
+
+export const light_blue = {
+  "$primary-light-blue": "#9cb6d6", // base
+  "$light-blue-1": "#f6f9ff", // lb 1
+  "$light-blue-2": "#ebf3ff", // lb 2
+  "$light-blue-3": "#d8e7fd", // lb 3
+  "$light-blue-4": "#d3dded", // lb 4
+  "$light-blue-5": "#889bb9", // prev: input-light | lb-5
+};
+
+export const grey = {
+  "$primary-grey": "#e3e3e3",
+  "$grey-1": "#f2f3f5",
+  "$grey-2": "#f5f5fa",
+  // "$grey-3": "#d3dded",
+  "$grey-4": "#e3e5e5", // prev: light-20
+  // "grey-5": "#dde1ea", // prev: secondary-soft-gray
+  "$grey-5": "#d3d2d2", // prev: soft-gray
+  "$grey-6": "#c1c0c0",
+};
+
+export const yellow = {
+  "$primary-light-yellow": "#fdf0cc",
+  "$light-yellow-1": "#fffef2",
+};
+
+export const black = {
+  "$primary-black": "#000000", //prev: $text-dark
+  "$black-1": "#0000004d",
+  "$black-2": "#707070",
+  "$black-3": "#6d6e71",
+  "$black-4": "rgba(45, 45, 45, 0.8)",
+  // "$text-gray-1": "#5d7183", // prev: text-gray-1
+};
+
+export const white = { $white: "#FFFFFF" };
+
+export const red = {
+  "$primary-red": "#fd5900",
+  "$red-1": "#ffe5e7",
+  "$red-2": "#ff6f6f", // prev: $red-secondary
+  "$red-3": "#fd3c4a",
+  "$red-4": "#ff0000", // prev: $error
+  "$red-5": "#e93d3d", // prev: $red
+};
+
+export const rose = {
+  "$primary-rose": "#d5596c",
+  "$rose-1": "#fff5f5",
+  "$rose-2": "#ffe6ea",
+  "$rose-3": "#ffc9c9",
+};
+
+export const green = {
+  "$primary-green": "#45aa6c",
+  "$green-1": "#eeffe0",
+  "$green-2": "#e7f5cf",
+  "$green-3": "#66be88",
+  "$green-4": "#2d412e",
+};
+
+export const turqoise = {
+  "$turqoise-primary": "#00beb7",
+  "$turqoise-1": "#25d366",
+  "$turqoise-2": "#02af84",
+  "$turqoise-3": "#1daa8d",
+};
+
+export const aqua = {
+  "$primary-aqua": "#94c9de",
+  "$aqua-1": "rgba(148, 201, 222, 0.35)",
+  "$aqua-2": "rgba(148, 201, 222, 0.4)",
+  "$aqua-3": "#cae4ee",
+};
+
+export const navy = {
+  "$primary-navy": "#6c88aa",
+  "$navy-1": "#4c70ac",
+  "$navy-2": "#323c47",
+};
+
+export const gradient = {
+  "$primary-gradient":
+    "linear-gradient(180deg, #ff9738 0%, #ff8743 100%), #ff9738",
+};
